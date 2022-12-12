7be9cd79747ccb057d7a2c978a2c68adf282eec8
HuziMuzi <laif17860@mail.ru>
2022-12-12 22:29:10 +0300

    add: сreate structure of project

---
+++ README.md
@@ -0,0 +1,10 @@
+### Installation: 
+
+`npm install`
+
+### Start:
+
+1. `npx react-native start`
+2. `npx react-native run-ios` or `npx react-native run-android`
+
+####  `.gitkeep` to save the folder structure.
---
+++ src/screens/Home.tsx
@@ -0,0 +1,21 @@
+import React  from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+
+
+export const Home = () => {
+  return (
+    <View style={styles.box}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  box: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+});
