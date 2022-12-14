c404204a278797383c7e6825742d2523daacca9b
HuziMuzi <laif17860@mail.ru>
2022-12-14 13:31:22 +0300

    add: testing-library and write first test App

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
 import React, {useEffect} from 'react';
 import {
+  Image,
   SafeAreaView,
-  StatusBar,
+  StatusBar, StyleSheet,
   useColorScheme,
 } from 'react-native';
-import SplashScreen from 'react-native-splash-screen'
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 import {Home} from "./src/screens/Home";
-
-
+import SplashScreen from "react-native-splash-screen";
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -30,10 +29,25 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+
         <Home/>
+        <Image
+            testID='image'
+            style={styles.tinyLogo}
+            source={{
+                uri: 'https://reactnative.dev/img/tiny_logo.png',
+            }}
+        />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+});
 
 
 export default App;
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,20 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render} from "@testing-library/react-native";
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+
+describe(App, () => {
+  it('should render React Native image', function () {
+    const {getByTestId} = render(<App/>);
+    const image  = getByTestId('image')
+    expect(image).toBeTruthy();
+  });
+})
