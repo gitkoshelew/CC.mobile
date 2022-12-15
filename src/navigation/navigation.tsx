1d672d0751e27de867fb77815ad52bc1cc2cb322
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-12-15 22:26:37 +0300

    add:react_navigation

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
 import {Home} from '../screens/Home';
 import {Example} from '../screens/Example';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
