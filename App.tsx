0d38a69a61b8f08c1e0c24cf7ec1f993444c43c7
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-12-16 14:50:27 +0300

    fix: firs review

--- App.tsx
+++ App.tsx
@@ -13,6 +13,8 @@ import SplashScreen from 'react-native-splash-screen';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Navigation from './src/navigation/navigation';
+import {Provider} from 'react-redux';
+import store from './src/bll/store/store';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -27,25 +29,27 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Navigation />
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
+      <Provider store={store}>
+        <Navigation />
+        <SafeAreaView style={backgroundStyle}>
+          <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
+          />
 
-        <Image
-          testID="image"
-          style={styles.tinyLogo}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-        />
-        <Text>
-          <AntDesign name="bars" style={{color: 'red', fontSize: 50}} />
-          <Entypo name="box" style={{color: 'red', fontSize: 50}} />
-        </Text>
-      </SafeAreaView>
+          <Image
+            testID="image"
+            style={styles.tinyLogo}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png',
+            }}
+          />
+          <Text>
+            <AntDesign name="bars" style={{color: 'red', fontSize: 50}} />
+            <Entypo name="box" style={{color: 'red', fontSize: 50}} />
+          </Text>
+        </SafeAreaView>
+      </Provider>
     </NavigationContainer>
   );
 };
