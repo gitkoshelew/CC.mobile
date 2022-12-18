921e43e46c2fcd30004161b73791d2fdd8311b49
HuziMuzi <laif17860@mail.ru>
2022-12-18 18:38:11 +0300

    add: storybook v6

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,13 +1,10 @@
 import 'react-native';
-import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-// import {render} from '@testing-library/react-native';
+// import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  // renderer.create(<App />);
 });
 
 jest.mock('react-native-splash-screen', () => {
@@ -16,11 +13,3 @@ jest.mock('react-native-splash-screen', () => {
     show: jest.fn(),
   };
 });
-
-// describe(App, () => {
-//   it('should render React Native image', function () {
-//     const {getByTestId} = render(<App />);
-//     const image = getByTestId('image');
-//     expect(image).toBeTruthy();
-//   });
-// });
