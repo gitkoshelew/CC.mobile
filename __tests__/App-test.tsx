165118409370e763799bf1d1cfc0749c5ce4fe27
HuziMuzi <laif17860@mail.ru>
2022-12-19 23:12:21 +0300

    fix: bag react vector icons

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,6 +2,7 @@ import 'react-native';
 
 // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
+// import App from '../App';
 
 it('renders correctly', () => {
   // renderer.create(<App />);
