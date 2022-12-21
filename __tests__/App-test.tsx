fe10a4110c4d97bdef9f7004f5ae41b57b056825
Julia Kovalchuk <yulya_kovalchuk_95@mail.ru>
2022-12-21 15:08:01 +0300

    fix: fix test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,12 +1,12 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import {render} from '@testing-library/react-native';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  render(<App />);
 });
 
 jest.mock('react-native-splash-screen', () => {
