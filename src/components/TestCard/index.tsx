ef4c329b137458a8a2ad84a400a4024e273bf8aa
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-12-23 17:15:04 +0300

    fix

--- src/components/TestCard/index.tsx
+++ src/components/TestCard/index.tsx
@@ -2,12 +2,16 @@ import {Text, View} from 'react-native';
 import {styles} from './styles';
 import {AppButton} from '../ui/AppButton';
 
-export const TestCard = () => {
+type ITestCard = {
+  onPress: () => void;
+};
+
+export const TestCard = (props: ITestCard) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Node.js</Text>
       <Text style={styles.description}>General questions about Node.js</Text>
-      <AppButton type="primary" title="Start" />
+      <AppButton type="primary" title="Start" onPress={props.onPress} />
     </View>
   );
 };
--- src/screens/Example.tsx
+++ src/screens/Example.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
+import {StyleSheet, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 export const Example = () => {
   return (
-    <View style={styles.box}>
+    <SafeAreaView style={styles.box}>
       <Text>Example screen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   box: {
+    paddingTop: 30,
     justifyContent: 'center',
     alignItems: 'center',
   },
