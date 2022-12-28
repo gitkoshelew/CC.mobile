c75a686a7f2f2157ff474ede37771ae728b99d89
HuziMuzi <laif17860@mail.ru>
2022-12-28 13:47:52 +0300

    fix: font family

--- src/components/ui/ReadyStyles/Boxes/index.ts
+++ src/components/ui/ReadyStyles/Boxes/index.ts
@@ -7,7 +7,7 @@ const TextBox = styled.Text`
   margin-bottom: 8px;
   margin-left: 8px;
   font-size: 16px;
-  font-family: 'Montserrat-Regular', sans-serif;
+  font-family: 'Montserrat-Regular';
 `;
 
 export {BlockBox, TextBox};
