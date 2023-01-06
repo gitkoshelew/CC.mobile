b3031891a9aae3c955ed26b10ce217c9da211bea
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2023-01-06 17:42:52 +0300

    Merge branch 'develop' into add_components_in_storybook

--- src/components/SwitchSelectors/tests/SwitchSelectors.test.tsx
+++ src/components/SwitchSelectors/tests/SwitchSelectors.test.tsx
@@ -2,8 +2,7 @@ import {fireEvent, render} from '@testing-library/react-native';
 import {SwitchSelectors} from '../index';
 
 test('Should render switchSelectors list', async () => {
-  const {getByText, toJSON} = render(<SwitchSelectors type="level" />);
+  const {getByText} = render(<SwitchSelectors type="level" />);
   const text = getByText('Easy');
   fireEvent.changeText(text);
-  expect(toJSON()).toMatchSnapshot();
 });
--- src/components/TestCard/tests/testCard.test.tsx
+++ src/components/TestCard/tests/testCard.test.tsx
@@ -2,8 +2,7 @@ import {fireEvent, render} from '@testing-library/react-native';
 import {TestCard} from '../index';
 
 test('Should render testCard list', async () => {
-  const {getByText, toJSON} = render(<TestCard onPress={() => {}} />);
+  const {getByText} = render(<TestCard onPress={() => {}} />);
   const button = getByText('Start');
   fireEvent.press(button);
-  expect(toJSON()).toMatchSnapshot();
 });
