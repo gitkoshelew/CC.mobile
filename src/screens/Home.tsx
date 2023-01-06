e2342dc42668d23f0e329994842e41a3164b0574
HuziMuzi <laif17860@mail.ru>
2023-01-06 21:54:33 +0300

    refactor: screen TestsList

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,5 @@
 import {StyleSheet, View} from 'react-native';
 import {Header} from '../components/Header';
-import {TestCard} from '../components/TestCard';
-import {Sort} from '../components/Sort';
-import {Tabs} from '../components/Tabs';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {CustomTextInput} from '../components/ui/CustomTextInput';
 
@@ -10,9 +7,6 @@ export const Home = () => {
   return (
     <SafeAreaView style={styles.box}>
       <Header />
-      <Tabs />
-      <Sort />
-      <TestCard onPress={() => {}} />
       <View style={styles.inputBox}>
         <CustomTextInput onChangeText={() => {}} value={'Hello world'} />
       </View>
