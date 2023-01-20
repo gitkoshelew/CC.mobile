20acd2df656c9ee15b4069aca10bd773f9e507fc
Julia Kovalchuk <yulya_kovalchuk_95@mail.ru>
2023-01-20 11:51:42 +0300

    feat: add test result component

--- src/components/CircularResultBar/index.tsx
+++ src/components/CircularResultBar/index.tsx
@@ -1,6 +1,7 @@
 import CircularProgress from 'react-native-circular-progress-indicator';
 import {Color} from '@theme/colors';
 import {StyleSheet} from 'react-native';
+import {ViewCenter} from '../ui/ReadyStyles/Containers';
 
 interface IProps {
   result: number;
@@ -8,17 +9,19 @@ interface IProps {
 
 export const CircularResultBar = ({result}: IProps) => {
   return (
-    <CircularProgress
-      value={result}
-      radius={83}
-      progressValueStyle={styles.progressValueStyle}
-      valueSuffix={'%'}
-      duration={1500}
-      activeStrokeWidth={26}
-      activeStrokeColor={Color.BlueLight}
-      inActiveStrokeWidth={26}
-      inActiveStrokeColor={Color.Gray}
-    />
+    <ViewCenter>
+      <CircularProgress
+        value={result}
+        radius={83}
+        progressValueStyle={styles.progressValueStyle}
+        valueSuffix={'%'}
+        duration={1500}
+        activeStrokeWidth={26}
+        activeStrokeColor={Color.BlueLight}
+        inActiveStrokeWidth={26}
+        inActiveStrokeColor={Color.Gray}
+      />
+    </ViewCenter>
   );
 };
 
---
+++ src/components/ui/SmallButton/index.tsx
@@ -0,0 +1,28 @@
+import {TouchableOpacity} from 'react-native';
+import {Box} from './styles';
+import {Color} from '@theme/colors';
+import {IButtonProps} from '@customTypes/SmallButton-types';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+export const SmallButton = ({type, onPress, ...props}: IButtonProps) => {
+  return (
+    <Box>
+      <TouchableOpacity {...props} onPress={onPress}>
+        {type === 'check' && (
+          <Ionicons
+            name={'shield-checkmark'}
+            size={17}
+            color={Color.GrayMedium}
+          />
+        )}
+        {type === 'edit' && (
+          <MaterialIcons name={'edit'} size={17} color={Color.GrayMedium} />
+        )}
+        {type === 'delete' && (
+          <MaterialIcons name={'delete'} size={17} color={Color.GrayMedium} />
+        )}
+      </TouchableOpacity>
+    </Box>
+  );
+};
