89923d28659a0e180db24893482919e05944b6ab
HuziMuzi <laif17860@mail.ru>
2023-01-25 15:28:53 +0300

    fix: merge conflict

---
+++ src/dal/quizzesAPI.ts
@@ -0,0 +1,20 @@
+import {instance} from '@src/dal/instance';
+import {
+  addQuestionToQuiz,
+  createTestRequestQuiz,
+} from '@customTypes/quizzesAPI-types';
+
+export const quizzesAPI = {
+  getQuiz() {
+    return instance.get('/quiz');
+  },
+  createQuiz(params: createTestRequestQuiz) {
+    return instance.post('/quiz', params);
+  },
+  getQuizQuestions(id: number) {
+    return instance.get(`/quiz/${id}`);
+  },
+  addQuestionToQuiz(params: addQuestionToQuiz) {
+    return instance.put('quiz/add/', params);
+  },
+};
