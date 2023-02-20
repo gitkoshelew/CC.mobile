ff998222ca288ca69ff6a394e5e9cde129fa957c
HuziMuzi <laif17860@mail.ru>
2023-02-20 12:52:57 +0300

    fix: import

--- src/screens/CreateQuiz/index.tsx
+++ src/screens/CreateQuiz/index.tsx
@@ -4,7 +4,6 @@ import {ViewFlex} from '@src/components/ui/ReadyStyles/Containers';
 import {RootCreateQuizParamsList} from '@customTypes/navigation-types';
 import {QuizSettingsContainer} from '@src/screens/CreateQuiz/components/QuizSettings/QuizSettingsContainer';
 import {QuestionsSettingsContainer} from '@src/screens/CreateQuiz/components/QuestionsSettings/QuestionsSettingsContainer';
-import {MainCreation} from '@src/screens/CreateQuiz/components/MainCreation/index';
 
 const Stack = createNativeStackNavigator<RootCreateQuizParamsList>();
 
@@ -14,7 +13,6 @@ export const CreateTest = () => {
       <Stack.Navigator>
         <Stack.Screen name={ScreenList.QUIZ_SET} component={QuizSettingsContainer} />
         <Stack.Screen name={ScreenList.QUESTIONS_SET} component={QuestionsSettingsContainer} />
-        <Stack.Screen name={ScreenList.MAIN_CREATION} component={MainCreation} />
       </Stack.Navigator>
     </ViewFlex>
   );
