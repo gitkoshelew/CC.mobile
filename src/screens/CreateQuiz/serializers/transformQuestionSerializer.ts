0cb60029928e44ecfdb685193a6c548beef4294c
HuziMuzi <laif17860@mail.ru>
2023-03-14 15:14:17 +0300

    fix: naming

--- src/screens/CreateQuiz/serializers/transformQuestionSerializer.ts
+++ src/screens/CreateQuiz/serializers/transformQuestionSerializer.ts
@@ -1,10 +1,10 @@
 import {DefaultTimeType, transformTime} from '@src/utils/transformTime';
 import {questionType} from '@customTypes/quiz-types';
 import {optionsType, transformFormatOptions} from '@src/utils/transformFormatOptions';
-import {OmitObjectProperties} from '@src/utils/omitObjectProperties';
+import {omitObjectProperties} from '@src/utils/omitObjectProperties';
 
 export const transformQuestionSerializer = (currentQuestion: questionType) => {
-  const {timer, ...rest} = OmitObjectProperties(
+  const {timer, ...rest} = omitObjectProperties(
     currentQuestion,
     'id',
     'Quiz_Question',
--- src/utils/omitObjectProperties.ts
+++ src/utils/omitObjectProperties.ts
@@ -1,4 +1,4 @@
-export const OmitObjectProperties = (object: object, ...props: string[]) => {
+export const omitObjectProperties = (object: object, ...props: string[]) => {
   const result = {...object};
   props.forEach(function (prop) {
     delete result[prop as keyof object];
