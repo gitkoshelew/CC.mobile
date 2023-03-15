16e436df5828b35c2cc2389865a36f997ee35a3f
HuziMuzi <laif17860@mail.ru>
2023-03-15 12:25:43 +0300

    feat: optimizing component rendering

--- src/screens/CreateQuiz/components/QuestionsSettings/QuestionsSettings.tsx
+++ src/screens/CreateQuiz/components/QuestionsSettings/QuestionsSettings.tsx
@@ -1,6 +1,6 @@
 import {QuestionsTabs} from '@src/components/QuestionsTabs';
 import {KeyboardAvoidingView, Platform, View} from 'react-native';
-import {useCallback, useContext, useState} from 'react';
+import {memo, useCallback, useContext, useMemo, useState} from 'react';
 import {styles} from './styles';
 import {questionType} from '@customTypes/quiz-types';
 import {CustomModal} from '@src/components/ui/Modal/index';
@@ -28,110 +28,116 @@ type QuestionsSettingsPropsType = {
 
 const Tab = createMaterialTopTabNavigator();
 
-export const QuestionsSettings = ({
-  topicId,
-  questions,
-  idNewQuiz,
-  changeQuestions,
-  numberOfQuestions,
-}: QuestionsSettingsPropsType) => {
-  const {t} = useTranslation(['createQuestion', 'labelsNavigation']);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const theme = useContext(ThemeContext);
+export const QuestionsSettings = memo(
+  ({
+    topicId,
+    questions,
+    idNewQuiz,
+    changeQuestions,
+    numberOfQuestions,
+  }: QuestionsSettingsPropsType) => {
+    const {t} = useTranslation(['createQuestion', 'labelsNavigation']);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const theme = useContext(ThemeContext);
 
-  const currentQuestion = questions[currentQuestionIndex]
-    ? transformQuestionSerializer(questions[currentQuestionIndex])
-    : {
-        ...getNewQuestion,
-        topicId,
-      };
+    const currentQuestion = useMemo(
+      () =>
+        questions[currentQuestionIndex]
+          ? transformQuestionSerializer(questions[currentQuestionIndex])
+          : {
+              ...getNewQuestion,
+              topicId,
+            },
+      [currentQuestionIndex, questions, topicId],
+    );
 
-  const handlerCurrentQuestion = useCallback(
-    (index: number) => {
-      if (
-        (!currentQuestion.title && index > questions.length - 1) ||
-        index - questions.length > 0
-      ) {
-        setIsModalVisible(true);
-        return;
-      }
+    const handlerCurrentQuestion = useCallback(
+      (index: number) => {
+        if (
+          (!currentQuestion.title && index > questions.length - 1) ||
+          index - questions.length > 0
+        ) {
+          setIsModalVisible(true);
+          return;
+        }
 
-      setCurrentQuestionIndex(index);
-    },
-    [currentQuestion.title, questions.length],
-  );
+        setCurrentQuestionIndex(index);
+      },
+      [currentQuestion.title, questions.length],
+    );
 
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      keyboardVerticalOffset={keyboardVerticalOffset}
-      style={styles().wrapper}>
-      <View style={styles().container}>
-        <ExitButton />
-        <CustomModal
-          isModalVisible={isModalVisible}
-          onPress={setIsModalVisible}
-          text={t('First create a question')}
-        />
-        <ViewFlex>
-          <View style={styles().inner}>
-            <View style={styles().questionTabsContainer}>
-              <QuestionsTabs
-                activeTab={currentQuestionIndex}
-                numberOfQuestions={numberOfQuestions}
-                amountFilledQuestion={questions.length}
-                onPressCurrentQuestion={handlerCurrentQuestion}
-              />
-            </View>
-            <View style={styles().tabsContainer}>
-              <Tab.Navigator
-                screenOptions={{
-                  tabBarStyle: styles(theme).tabBarStyle,
-                  tabBarIndicatorContainerStyle: styles().tabBarIndicatorContainerStyle,
-                  tabBarLabelStyle: styles(theme).tabBarLabelStyle,
-                  lazy: true,
-                  lazyPlaceholder: () => LazyLoadingLayout(theme),
-                }}>
-                <Tab.Screen
-                  name={ScreenList.CREATE_QUESTION}
-                  options={{
-                    tabBarLabel: t('questionsSettings.Create question', {
-                      ns: 'labelsNavigation',
-                    })!,
-                  }}
-                  children={() => (
-                    <CreateQuestionContainer
-                      quizId={idNewQuiz}
-                      changeQuestions={changeQuestions}
-                      currentQuestion={currentQuestion}
-                      currentQuestionIndex={currentQuestionIndex}
-                      changeCurrentQuestionIndex={setCurrentQuestionIndex}
-                    />
-                  )}
+    return (
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={keyboardVerticalOffset}
+        style={styles().wrapper}>
+        <View style={styles().container}>
+          <ExitButton />
+          <CustomModal
+            isModalVisible={isModalVisible}
+            onPress={setIsModalVisible}
+            text={t('First create a question')}
+          />
+          <ViewFlex>
+            <View style={styles().inner}>
+              <View style={styles().questionTabsContainer}>
+                <QuestionsTabs
+                  activeTab={currentQuestionIndex}
+                  numberOfQuestions={numberOfQuestions}
+                  amountFilledQuestion={questions.length}
+                  onPressCurrentQuestion={handlerCurrentQuestion}
                 />
-                <Tab.Screen
-                  name={ScreenList.LIST_QUESTIONS}
-                  options={{
-                    tabBarLabel: t('questionsSettings.List questions', {
-                      ns: 'labelsNavigation',
-                    })!,
-                  }}
-                  children={() => (
-                    <ListQuestionsContainer
-                      quizId={idNewQuiz}
-                      currentQuizQuestions={questions}
-                      changeQuestions={changeQuestions}
-                      currentQuestionIndex={currentQuestionIndex}
-                      changeCurrentQuestionIndex={setCurrentQuestionIndex}
-                    />
-                  )}
-                />
-              </Tab.Navigator>
+              </View>
+              <View style={styles().tabsContainer}>
+                <Tab.Navigator
+                  screenOptions={{
+                    tabBarStyle: styles(theme).tabBarStyle,
+                    tabBarIndicatorContainerStyle: styles().tabBarIndicatorContainerStyle,
+                    tabBarLabelStyle: styles(theme).tabBarLabelStyle,
+                    lazy: true,
+                    lazyPlaceholder: () => LazyLoadingLayout(theme),
+                  }}>
+                  <Tab.Screen
+                    name={ScreenList.CREATE_QUESTION}
+                    options={{
+                      tabBarLabel: t('questionsSettings.Create question', {
+                        ns: 'labelsNavigation',
+                      })!,
+                    }}
+                    children={() => (
+                      <CreateQuestionContainer
+                        quizId={idNewQuiz}
+                        changeQuestions={changeQuestions}
+                        currentQuestion={currentQuestion}
+                        currentQuestionIndex={currentQuestionIndex}
+                        changeCurrentQuestionIndex={setCurrentQuestionIndex}
+                      />
+                    )}
+                  />
+                  <Tab.Screen
+                    name={ScreenList.LIST_QUESTIONS}
+                    options={{
+                      tabBarLabel: t('questionsSettings.List questions', {
+                        ns: 'labelsNavigation',
+                      })!,
+                    }}
+                    children={() => (
+                      <ListQuestionsContainer
+                        quizId={idNewQuiz}
+                        currentQuizQuestions={questions}
+                        changeQuestions={changeQuestions}
+                        currentQuestionIndex={currentQuestionIndex}
+                        changeCurrentQuestionIndex={setCurrentQuestionIndex}
+                      />
+                    )}
+                  />
+                </Tab.Navigator>
+              </View>
             </View>
-          </View>
-        </ViewFlex>
-      </View>
-    </KeyboardAvoidingView>
-  );
-};
+          </ViewFlex>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  },
+);
