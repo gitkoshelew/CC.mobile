8290a8367c977565c74f0ca9b2cfe497f2e735e5
HuziMuzi <laif17860@mail.ru>
2023-03-26 15:02:57 +0300

    fix: remove memo

--- src/screens/PassingQuiz/components/QuizList/QuizListContainer.tsx
+++ src/screens/PassingQuiz/components/QuizList/QuizListContainer.tsx
@@ -1,4 +1,4 @@
-import {memo, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useAppDispatch, useAppSelector} from '@hooks/hooks';
 import {getQuizzes} from '@src/bll/quizReducer';
 import {getTopics} from '@src/screens/CreateQuiz/services/services';
@@ -12,39 +12,41 @@ type QuizListPropsType = {
   onPressStartTesting: (id: number) => void;
 };
 
-export const QuizListContainer = memo(
-  ({onPressStartTesting, isModalVisible, setIsModalVisible}: QuizListPropsType) => {
-    const dispatch = useAppDispatch();
-    const isFetching = useAppSelector(state => state.app.isFetching);
-    const [quizzesData, setQuizzesData] = useState<getQuizResponseType[]>([]);
-    const [topics, setTopics] = useState<string[]>(['all']);
+export const QuizListContainer = ({
+  onPressStartTesting,
+  isModalVisible,
+  setIsModalVisible,
+}: QuizListPropsType) => {
+  const dispatch = useAppDispatch();
+  const isFetching = useAppSelector(state => state.app.isFetching);
+  const [quizzesData, setQuizzesData] = useState<getQuizResponseType[]>([]);
+  const [topics, setTopics] = useState<string[]>(['all']);
 
-    useEffect(() => {
-      dispatch(getQuizzes())
-        .unwrap()
-        .then(res => {
-          setQuizzesData(res);
-        });
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getQuizzes())
+      .unwrap()
+      .then(res => {
+        setQuizzesData(res);
+      });
+  }, [dispatch]);
 
-    useEffect(() => {
-      dispatch(getTopics())
-        .unwrap()
-        .then(res => {
-          setTopics(['All', ...res.map((el: TopicType) => el.title)]);
-        });
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getTopics())
+      .unwrap()
+      .then(res => {
+        setTopics(['All', ...res.map((el: TopicType) => el.title)]);
+      });
+  }, [dispatch]);
 
-    return (
-      <ScreenLayout isFetching={isFetching}>
-        <QuizList
-          isModalVisible={isModalVisible}
-          setIsModalVisible={setIsModalVisible}
-          onPressStartTesting={onPressStartTesting}
-          quizzesData={quizzesData}
-          topics={topics}
-        />
-      </ScreenLayout>
-    );
-  },
-);
+  return (
+    <ScreenLayout isFetching={isFetching}>
+      <QuizList
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        onPressStartTesting={onPressStartTesting}
+        quizzesData={quizzesData}
+        topics={topics}
+      />
+    </ScreenLayout>
+  );
+};
